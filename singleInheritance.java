bf5f520ebe4247d675f3e6e27285a63ae5b09dff
varungarg2796 <varungarg2796@hotmail.com>
2018-10-01 18:19:51 +0100

    Changes

--- singleInheritance.java
+++ singleInheritance.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class ClassA{
     public void displayA(){
         System.out.println("This is from A");
@@ -9,9 +11,25 @@ class ClassB extends ClassA{
         System.out.println("This is from B");
     }
     public static void main(String[] args){
-        ClassB b = new ClassB();
-        b.displayA();
-        b.displayB();
+        int[] mymarks = new int[10];
+
+int total = 0;
+
+Scanner in = new Scanner(System.in);
+
+for (int cnt = 1; cnt <= 10; cnt++)
+
+{
+
+   System.out.print("Enter the marks: ");
+
+   mymarks[cnt] = in.nextInt();
+
+   total = total + mymarks[cnt];
+
+}
+
+System.out.println(total);
 
     }
 }
