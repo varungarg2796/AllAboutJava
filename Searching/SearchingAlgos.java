65f839e772ad96e40e7e848b1ba3e6e5f1eea628
varungarg2796 <varungarg2796@hotmail.com>
2018-10-16 01:28:20 +0100

    Binary Search

--- Searching/SearchingAlgos.java
+++ Searching/SearchingAlgos.java
@@ -1,4 +1,4 @@
-
+import java.lang.*;
 public class SearchingAlgos{
 
     //A BASIC LINEAR SEARCH
@@ -12,8 +12,30 @@ public class SearchingAlgos{
         return -1;
     }
 
+
+    //BINARY SEARCH
+    //We divide the array into half after each iteration
+    public static int binarySearch(int[] arr, int num){
+        int start = 0;
+        int end = arr.length - 1;
+        int middle = (int)Math.floor(start + end) / 2;
+        while(arr[middle]!= num){
+            if(num < arr[middle]){
+                end = middle - 1;
+            } else{
+                start = middle + 1;
+            }
+            middle = (int)Math.floor(start + end) / 2;
+        }
+        if(arr[middle] == num){
+            System.out.println("Found at index " + middle);
+            return 1;
+        }
+        return -1;
+    }
     public static void main (String[] args){
-        int a[] = {1,3,4,5,6};
-        System.out.println(linearSearch(a, 5));
+        int a[] = {1,3,4,5,6,7,8,9};
+        //System.out.println(linearSearch(a, 5));
+        System.out.println(binarySearch(a, 8));
     }
 }
