20ca9130286c37145273742b08247cec8d585a3c
varungarg2796 <varungarg2796@hotmail.com>
2018-10-19 02:04:22 +0100

    Linked List imlementation

---
+++ DataStructures/LinkUse.java
@@ -0,0 +1,12 @@
+public class LinkUse{
+    public static void main( String[] args)
+    {
+        LinkedList list = new LinkedList();
+        //LinkedList list = new LinkedList<>();
+        //list.add(1);
+        list.insert(5);
+        list.insert(100);
+        list.insert(200);
+        list.show();
+    }
+}
--- DataStructures/LinkedList.java
+++ DataStructures/LinkedList.java
@@ -1,25 +1,42 @@
-public class LinkedList{
-    private int val;
-    private Integer next;
-    public LinkedList(int val){
-        System.out.println(val);
-        this.val=val;
-        this.next=null;
+//import java.util.LinkedList;
+
+
+public class LinkedList
+{
+    Node head;
+
+    public void insert (int data) //inserting at end
+    {
+        //Initialize the node and put data in it
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        //Check if it is the first element being inserted?
+        if(head == null)
+        {
+            head = node;
+        }
+        else //If not, then
+        {
+            Node n = head; //create a temporary node of head
+            while(n.next != null)
+            {
+                n = n.next; //keep checking and moving forward till the reference points to a null value that is, you reach the last element
+            }
+            n.next = node; //point the null reference to the new node
+        }
     }
-    public static void main(String[] args){
-        LinkedList node = new LinkedList(100);
-        SinglyLinkedList list = new SinglyLinkedList();
+
+    public void show()
+    {
+        Node n = head;
+        while(n!=null)
+        {
+            System.out.println(n.data);
+            n= n.next;
+        }
     }
 }
 
-class SinglyLinkedList{
-    public Integer head;
-    public Integer tail;
-    public int length;
-    public SinglyLinkedList(){
-        System.out.println("LOL");
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-}
+
