b2e48bea021aeb09ac6f71bd3a6f02d993f8952c
varungarg2796 <varungarg2796@hotmail.com>
2018-10-24 00:43:28 +0100

    Jumping clouds

--- HackerRank Problems/jumpingClouds.java
+++ HackerRank Problems/jumpingClouds.java
@@ -11,19 +11,31 @@ public class jumpingClouds {
     // Complete the jumpingOnClouds function below.
     static int jumpingOnClouds(int[] c) {
 
+        int jump = 0;
 
+        int i = 0;
+        while( i < c.length-1 ){
+            if( i+2 < c.length && c[i+2] ==0 ){
+                jump ++;
+                i= i+2;
+            } else {
+                i++;
+                jump++;
+            }
+        }
+        return jump;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int[] c = {0, 0 ,0, 0 ,1, 0};
+        
+        //int[] c = {0, 0 ,0, 1 ,0, 0};
 
-        int[] c = new int[n];
-
+        
         int result = jumpingOnClouds(c);
         System.out.println(result);
 
