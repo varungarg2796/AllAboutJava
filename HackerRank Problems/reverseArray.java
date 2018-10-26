816d83ca76382fb4a776759364a3d4b1a525584c
varungarg2796 <varungarg2796@hotmail.com>
2018-10-26 19:54:29 +0100

    For practice

--- HackerRank Problems/reverseArray.java
+++ HackerRank Problems/reverseArray.java
@@ -1,3 +1,15 @@
+/**
+ https://www.hackerrank.com/challenges/30-arrays/problem
+
+ Sample Input
+4
+1 4 3 2
+
+Sample Output
+2 3 4 1
+ */
+
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
